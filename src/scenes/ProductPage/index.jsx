2348c0e06bd2d8c37614999d5dbeed881b408fc6
kamczill <kam134@interia.eu>
2023-07-09 11:44:12 +0200

    feat: add product button functionality for button and connect it with localstorage

--- src/scenes/ProductPage/index.jsx
+++ src/scenes/ProductPage/index.jsx
@@ -5,6 +5,7 @@ import axiosInstance from '../../axios/axios';
 const index = () => {
   const { id } = useParams();
   const [product, setProduct] = useState();
+  const [isInCart, setIsInCart] = useState()
   console.log(id)
 
   const getProduct = async () => {
@@ -13,7 +14,22 @@ const index = () => {
     }).then(res => {
       console.log(res.data)
       setProduct(res?.data)
+      checkProductCart(res?.data)
     })
+  }
+
+  const checkProductCart = (product) => {
+    const items = JSON.parse(localStorage.getItem('cart'))
+    if (items?.includes(product?.id)){
+      setIsInCart(true)
+    }
+  }
+
+  const addToCart = () => {
+    const items = JSON.parse(localStorage.getItem('cart')) || []
+    console.log(items)
+    localStorage.setItem('cart', JSON.stringify([...items,product?.id]));
+    setIsInCart(true)
   }
 
   useEffect(() => {
@@ -30,9 +46,12 @@ const index = () => {
             <p class='text-lg'>{product?.author}</p>
             <h3 class='font-ms font-bold text-xl pointer lg:hover:underline lg:hover:decoration-2'>{product?.title}</h3>
               <p class='text-2xl py-5'>{product?.net_price} zł</p>
-            {/* <div class='max-w-[357px] flex justify-between items-center px-3 pt-3'> */}
-            {/* </div> */}
-              <button class='w-full max-w-[357px] bg-green-500 py-2 px-4 text-white rounded active:scale-95'>Dodaj do koszyka</button>
+              {
+                isInCart ?
+                <button onClick={() => addToCart()} disabled class='w-full max-w-[357px] bg-green-300 py-2 px-4 text-white rounded'>Dodane do koszyka</button>:
+                <button onClick={() => addToCart()} class='w-full max-w-[357px] bg-green-500 py-2 px-4 text-white rounded active:scale-95'>Dodaj do koszyka</button>
+
+              }
             <div
               dangerouslySetInnerHTML={{__html: product?.description}}
               class='py-5'
