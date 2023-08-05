cfe4397d2b57d4f08119873876762cc53603983a
kamczill <kam134@interia.eu>
2023-08-05 19:52:33 +0200

    Remove Feature components inside Features

--- src/scenes/HomePage/Features.jsx
+++ src/scenes/HomePage/Features.jsx
@@ -7,21 +7,6 @@ const Features = () => {
     <div class='py-6'>
         <h2 class='text-3xl mt-4 text-center'>Dlaczego warto?</h2>
         <div class='flex flex-col gap-10 justify-center items-center mt-10 px-5 pb-8 lg:flex-row'>
-            <Feature 
-                img={featureAvatar}
-                headline='Lorem ipsum'
-                text='Lorem ipsum Lorem ipsum Lorem ipsum'
-            />
-            <Feature 
-                img={featureAvatar}
-                headline='Lorem ipsum'
-                text='Lorem ipsum Lorem ipsum Lorem ipsum'
-            />
-            <Feature 
-                img={featureAvatar}
-                headline='Lorem ipsum'
-                text='Lorem ipsum Lorem ipsum Lorem ipsum'
-            />
         </div>
     </div>
   )
