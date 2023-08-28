67ee090882b2f4b7fcab3cd2bc7e7876c37ece82
kamczill <kam134@interia.eu>
2023-08-28 19:32:50 +0200

    Update Features

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -6,7 +6,10 @@ const Feature = ({img, headline, text}) => {
     <div class='font-poppins flex flex-col justify-center items-center gap-3'>
         <div class='w-[80vw] max-w-[250px] relative'>
             <img src={shape} />
-            <img src={img}  class='w-[170px] absolute m-auto top-9 left-0 right-0'/>
+            {/* <img src= class='w-[170px] absolute m-auto top-9 left-0 right-0'/> */}
+            <div class='absolute inset-0 flex justify-center items-center'>
+              {img}
+            </div>
         </div>
         <div class='flex flex-col justify-center items-center'>
             <h3 class='font-bold text-2xl text-gray-600'>{headline}</h3>
--- src/scenes/HomePage/Features.jsx
+++ src/scenes/HomePage/Features.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import featureAvatar from './../../assets/feature-avatar-1.svg'
-
 import Feature from '../../components/Feature'
+
+import {AiOutlineFilePdf, AiOutlineArrowRight, AiOutlineArrowDown } from 'react-icons/ai'
+import {VscAccount } from 'react-icons/vsc'
+import {BsFillCartCheckFill } from 'react-icons/bs'
+
 const Features = () => {
   return (
-    <div class='py-6'>
-        <h2 class='text-3xl mt-4 text-center'>Dlaczego warto?</h2>
+    <div class='py-6 px-5 bg-gray-200'>
+        <h2 class='text-3xl mt-4 text-center'>Jak to działa?</h2>
+        <div className='flex justify-center'>
+        <p className='max-w-[1200px] text-center text-lg font-ms pt-10'>
+        Umożliwiamy nie tylko zakup e-booków i plików PDF z szerokiej gamy tytułów, ale także bezpośredni odczyt zawartości na platformie. Intuicyjny interfejs i zaawansowane funkcje sprawiają, że lektura staje się prawdziwą przyjemnością, łącząc zakupy i czytanie w jednym miejscu.
+        </p>
+        </div>
         <div class='flex flex-col gap-10 justify-center items-center mt-10 px-5 pb-8 lg:flex-row'>
+          <Feature headline='Załóż konto' text='W prosty sposób!' img={<VscAccount size={100} color={'#ffffff'}/>}/>
+          <AiOutlineArrowRight size={40} className='hidden lg:block' />
+          <AiOutlineArrowDown size={40} className='lg:hidden' />
+          <Feature headline='Złóż zamówienie' text='Realizowane natychmiast' img={<BsFillCartCheckFill size={100} color={'#ffffff'}/>}/>
+          <AiOutlineArrowRight size={40} className='hidden lg:block'/>
+          <AiOutlineArrowDown size={40} className='lg:hidden' />
+          <Feature headline='Korzystaj!' text='Pobierz lub Otwórz' img={<AiOutlineFilePdf size={100} color={'#ffffff'}/>}/>
         </div>
     </div>
   )
