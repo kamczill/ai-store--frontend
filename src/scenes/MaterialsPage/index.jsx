ab199e39cb5fff441f6f39f3ee16d24dfd26b9df
kamczill <kam134@interia.eu>
2023-10-14 12:52:58 +0200

    Refactor MaterialsPage for readability and performance

--- src/scenes/MaterialsPage/index.jsx
+++ src/scenes/MaterialsPage/index.jsx
@@ -1,9 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import ProductCard from '../../components/ProductCard'
-
-import axios from '../../axios/axios'
-import { TailSpin } from 'react-loader-spinner'
 import Loader from '../../components/Loader'
+import axios from '../../axios/axios'
 
 
 const index = () => {
@@ -12,16 +10,16 @@ const index = () => {
   const [loadedImagesCount, setLoadedImagesCount] = useState(0);
 
   const getAllMaterials = async () => {
-    await axios('users/me/products/', {
-      withCredentials: true
-    }).then(res => {
-      setProducts(res?.data)
-      console.log(res?.data)
-    }).catch( err => {
-      console.log(err)
-    })
-  }
-  
+    try {
+      const response = await axios.get('users/me/products/', {
+        withCredentials: true,
+      });
+      setProducts(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   
   const handleImageLoad = () => {
     setLoadedImagesCount(prevCount => prevCount + 1);
@@ -39,23 +37,15 @@ const index = () => {
 
   return (
     <>
-        <h1 class='text-center font-bold font-ms text-xl pt-6'>Moje materiały</h1>
-      {
-        isLoading 
-        ? <Loader />
-        : ''
-      }
-      <div className='mx-auto max-w-[1350px]'>
-      {
-          products?.length === 0 && <p>Brak kupionych materiałów</p>
-      }
-      <div class={` ${isLoading ? 'hidden': ''} pt-8 flex flex-col flex-wrap gap-8 items-center justify-center md:flex-row md:gap-8 md:justify-center md:items-center`}>
-        {
-          products?.map(product => <ProductCard product={product} onLoad={handleImageLoad} isBought/>)
-        }
+    <h1 className='text-center font-bold font-ms text-xl pt-6'>Moje materiały</h1>
+    {isLoading ? <Loader /> : null}
+    <div className='mx-auto max-w-[1350px]'>
+      {products.length === 0 && <p>Brak kupionych materiałów</p>}
+      <div className={`${isLoading ? 'hidden' : ''} pt-8 flex flex-col flex-wrap gap-8 items-center justify-center md:flex-row md:gap-8 md:justify-center md:items-center`}>
+        {products.map((product) => <ProductCard key={product.id} product={product} onLoad={handleImageLoad} isBought />)}
       </div>
-      </div>
-    </>
+    </div>
+  </>
   )
 }
 
